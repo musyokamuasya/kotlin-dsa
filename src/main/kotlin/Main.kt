320c41537b05fd905a5bfe1b0f72f7641df413e7
Musyoka Muasya <josephmuasya8@gmail.com>
2022-06-28 00:51:11 +0300

    Move german numbers from main class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,22 @@
 fun main(args: Array<String>) {
-        for (index in 0..10 step 2) {
-            when (index) {
-                0 -> println("Zero")
-                1 -> println("Eins")
-                2 -> println("Zwei")
-                3 -> println("Drei")
-                4 -> println("Vier")
-                5 -> println("Funf")
-                6 -> println("Sechs")
-                7 -> println("Sieben")
-                8 -> println("Acht")
-                9 -> println("Neun")
-                10 -> println("Zehn")
-                else -> println("Something bigger than four")
-            }
+     germanNumbers()
+}
+
+fun germanNumbers (){
+    for (index in 0..10 step 2) {
+        when (index) {
+            0 -> println("Zero")
+            1 -> println("Eins")
+            2 -> println("Zwei")
+            3 -> println("Drei")
+            4 -> println("Vier")
+            5 -> println("Funf")
+            6 -> println("Sechs")
+            7 -> println("Sieben")
+            8 -> println("Acht")
+            9 -> println("Neun")
+            10 -> println("Zehn")
+            else -> println("Something bigger than four")
         }
+    }
 }
