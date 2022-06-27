fd80f24ec8318afd82f2319a6bde456411e9e55d
Musyoka Muasya <josephmuasya8@gmail.com>
2022-06-28 02:12:15 +0300

    Example of mutable collections, access through index

--- src/main/kotlin/collections/Collections.kt
+++ src/main/kotlin/collections/Collections.kt
@@ -1,5 +1,17 @@
 package collections
 
 fun main(args: Array<String>) {
-    println("This is all about collections in Kotlin")
+/**
+* Lists - Ordered collection with access to elements by indices which reflect their position
+ * Set - Group of elements without repetition
+ * Map - Key value pairs. Uses unique keys which map to only one value
+* */
+    val names = mutableListOf<String>("Nashon", "Becky", "Esther", "Martha", "Bonke", "Ciox")
+    names.add("Mash")
+    names.add("Jere")
+    names.add("Jeff")
+    println(names)
+    for (index in names.indices){
+        println("Name at index $index is ${names[index]}")
+    }
 }
