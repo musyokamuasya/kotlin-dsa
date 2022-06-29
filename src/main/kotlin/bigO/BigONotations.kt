7d29b1592b0ecc40586b59de11b86cff3f185f3e
Musyoka Muasya <josephmuasya8@gmail.com>
2022-06-29 17:19:35 +0300

    Check time it took to run the two programs

--- src/main/kotlin/bigO/BigONotations.kt
+++ src/main/kotlin/bigO/BigONotations.kt
@@ -1,9 +1,13 @@
 package bigO
 
+import kotlin.system.measureTimeMillis
 
-    fun main(args: Array<String>){
-        checkFirstNames(listOf("Kenya", "Tanzania", "Uganda", "Namibia", "Lesotho", "South Africa", ))
-        checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
+
+fun main(args: Array<String>){
+    checkTimeMills()
+
+    //        checkFirstNames(listOf("Kenya", "Tanzania", "Uganda", "Namibia", "Lesotho", "South Africa", ))
+    //        checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
     }
     /**
      * An example of linear function. It works irrespective of the size of the names, since it checks only the first name
@@ -19,3 +23,19 @@ package bigO
             println("No names")
         }
     }
+
+//Check the time the function runs in milliseconds
+
+    fun checkTimeMills (){
+        val time1 = measureTimeMillis {
+            checkFirstNames(listOf("Kenya", "Tanzania", "Uganda", "Namibia", "Lesotho", "South Africa"))
+//            checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
+        }
+        println(time1)
+        val time2 = measureTimeMillis {
+//            checkFirstNames(listOf("Kenya", "Tanzania", "Uganda", "Namibia", "Lesotho", "South Africa"))
+            checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
+        }
+        println(time2)
+    }
+
