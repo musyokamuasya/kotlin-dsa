fb01f05da32103a344c89705311948022c2e3c67
Musyoka Muasya <josephmuasya8@gmail.com>
2022-06-29 22:49:41 +0300

    Linear function, O(n)

--- src/main/kotlin/bigO/BigONotations.kt
+++ src/main/kotlin/bigO/BigONotations.kt
@@ -10,7 +10,7 @@ fun main(args: Array<String>){
     //        checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
     }
     /**
-     * An example of linear function. It works irrespective of the size of the names, since it checks only the first name
+     * An example of constant function. It works irrespective of the size of the names, since it checks only the first name
      *
      * No matter the size of the list, it prints the first name
      * */
@@ -23,19 +23,34 @@ fun main(args: Array<String>){
             println("No names")
         }
     }
+    /**
+     * An example of linear function. Represented by big-O 0(n)
+     * */
+    fun getAllNames(names: List<String>){
+        for(name in names){
+            println(name)
+        }
+    }
 
 //Check the time the function runs in milliseconds
 
     fun checkTimeMills (){
-        val time1 = measureTimeMillis {
-            checkFirstNames(listOf("Kenya", "Tanzania", "Uganda", "Namibia", "Lesotho", "South Africa"))
-//            checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
-        }
-        println(time1)
-        val time2 = measureTimeMillis {
+//        val time1 = measureTimeMillis {
 //            checkFirstNames(listOf("Kenya", "Tanzania", "Uganda", "Namibia", "Lesotho", "South Africa"))
-            checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
+////            checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
+//        }
+//        println(time1)
+//        val time2 = measureTimeMillis {
+////            checkFirstNames(listOf("Kenya", "Tanzania", "Uganda", "Namibia", "Lesotho", "South Africa"))
+//            checkFirstNames(listOf("USA", "Canada", "GB", "Italy", "France", "Croatia"))
+//        }
+//        println(time2)
+
+
+//        Linear function
+       val time = measureTimeMillis {
+            getAllNames(listOf("Kenya", "Tanzania", "Uganda", "Namibia", "Lesotho", "South Africa"))
         }
-        println(time2)
+        print(time)
     }
 
