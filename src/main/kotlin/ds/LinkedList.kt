85cd8e02fd2a128385fb9607306d821d7c890e4f
Musyoka Muasya <josephmuasya8@gmail.com>
2022-06-30 15:21:20 +0300

    Linked list class to handle node instantiation

--- src/main/kotlin/ds/LinkedList.kt
+++ src/main/kotlin/ds/LinkedList.kt
@@ -19,7 +19,39 @@ data class Node<T>(var value:T, var next: Node<T>? = null){
     }
 }
 
+/**
+ * The above method is hard to create and manage linked list.
+ *
+ * Ideal method involves using a class to manage the linkedlist
+ * */
+class LinkedList <T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var size = 0
+
+    private  fun isEmpty(): Boolean{
+        return size ==0
+    }
+
+    override fun toString(): String {
+        if (isEmpty()){
+            return "List is empty"
+        }
+        return head.toString()
+    }
+//    Push method to add values at the tail of the linkedlist
+    fun push(value: T){
+        head = Node(value =value, next = head)
+//    Check if there is no value at the tail, tail value will be the head value
+        if(tail == null){
+            tail = head
+        }
+    }
+}
+
 fun main(){
+    val linkedList = LinkedList<Int>()
+    val node4 = linkedList.push(4)
     val node1 = Node(value = 1)
     val node2 = Node(value = 2)
     val node3 = Node(value = 3)
@@ -27,5 +59,7 @@ fun main(){
     node1.next = node2
     node2.next = node3
 
+
+//    println(node4)
     println(node1)
 }
