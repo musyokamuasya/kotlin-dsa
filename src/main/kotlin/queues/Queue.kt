7768f8a9331280e4d48b3a8d6201f34a8735bd0d
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-02 02:45:43 +0300

    Queue interface with common functions of enqueue, dequeue

---
+++ src/main/kotlin/queues/Queue.kt
@@ -0,0 +1,15 @@
+package queues
+
+interface Queue <T> {
+    fun enqueue(item: T) : Boolean
+
+    fun dequeue(): T?
+
+    fun peek(): T
+
+    val count: Int
+        get
+
+    val isEmpty : Boolean
+        get() = count == 0
+}
