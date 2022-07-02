0712e330b4951a15d269ca96f4a97d77c31dfd5d
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-02 04:53:34 +0300

    Construct beverages tree

--- src/main/kotlin/trees/basic/TreeNode.kt
+++ src/main/kotlin/trees/basic/TreeNode.kt
@@ -19,12 +19,44 @@ class TreeNode<T>(val value: T) {
        }
     }
 
-    fun makeBeveragesTree(){
-
-    }
 }
 /**
  * Type alias gives another name to a class/object.
  * Mostly used when you do not want to use the actual name of the object, or the name is long
  * */
 typealias Visitor<T> = (TreeNode<T>) -> Unit
+
+fun makeBeveragesTree(): TreeNode<String>{
+    val tree = TreeNode("Beverages")
+    val hot = TreeNode("Hot")
+    val cold = TreeNode("Cold")
+    val coffee = TreeNode("Coffee")
+    val tea = TreeNode("Tea")
+    val chocolate = TreeNode("Chocolate")
+    val blackTea = TreeNode("Black tea")
+    val greenTea = TreeNode("Green Tea")
+    val chaiTea = TreeNode("Chai Tea")
+    val soda = TreeNode("Soda")
+    val milk = TreeNode("Milk")
+    val juice = TreeNode("Juice")
+    val lemon = TreeNode("Lemon")
+    val mango = TreeNode("Mango")
+    val strawberry = TreeNode("Strawberry")
+
+    tree.add(hot)
+    tree.add(cold)
+    hot.add(coffee)
+    hot.add(chocolate)
+    hot.add(tea)
+    cold.add(soda)
+    cold.add(juice)
+    cold.add(milk)
+    tea.add(blackTea)
+    tea.add(greenTea)
+    tea.add(chaiTea)
+    juice.add(lemon)
+    juice.add(mango)
+    juice.add(strawberry)
+
+    return tree
+}
