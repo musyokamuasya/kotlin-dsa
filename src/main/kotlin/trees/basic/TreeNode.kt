016f5cdf9f9c57f62b9883cf5bce19a91938f006
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-02 04:28:13 +0300

    Type alias for the node tree to make it easy to use

--- src/main/kotlin/trees/basic/TreeNode.kt
+++ src/main/kotlin/trees/basic/TreeNode.kt
@@ -5,5 +5,9 @@ class TreeNode<T>(val value: T) {
     fun add(child: TreeNode<T>){
         children.add(child)
     }
-
 }
+/**
+ * Type alias gives another name to a class/object.
+ * Mostly used when you do not want to use the actual name of the object, or the name is long
+ * */
+typealias Visitor<T> = (TreeNode<T>) -> Unit
