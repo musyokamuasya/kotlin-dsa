faa3ad336c00b416ea6604449718991f9c14d9bc
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-04 02:27:12 +0300

    Insert a binary tree

---
+++ src/main/kotlin/binary_search/BinarySearchTree.kt
@@ -0,0 +1,38 @@
+package binary_search
+
+import trees.binary.BinaryNode
+
+/**
+ * Binary Search
+ * Allows fast lookup, insert and removal operations
+ * `Rules` -> Item on the left is less than the root node
+ *         -> Item on the right is greater than/ Equal to root node
+ *  Removes the needs of doing unnecessary checks
+ *  Contains O(log n)
+ **/
+class BinarySearchTree <T : Comparable<T>>(){
+//    Ensure the values in the binary search tree are comparable
+    var root : BinaryNode<T>? = null
+    override fun toString(): String = root?.toString() ?: "Empty tree"
+
+    fun insert(value : T){
+        root = insert(root, value)
+    }
+
+    private fun insert (
+        node: BinaryNode<T>?,
+        value: T) : BinaryNode<T>{
+//        If the root is empty, add the node to the root node
+        node?: BinaryNode(value)
+//      When the value is less than the node value, inset on the left
+        if (value < node!!.value){
+            node.leftChild = insert(node.leftChild, value)
+        }
+        else{
+//      When the value is greater/ equal to node value, insert on the right
+            node.rightChild = insert(node.rightChild, value)
+        }
+
+        return node
+    }
+}
