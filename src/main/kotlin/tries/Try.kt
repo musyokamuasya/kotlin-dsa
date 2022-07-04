4b52292b249b4d3d9d439cf7a86fb4f6210f1de0
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-04 22:34:24 +0300

    Add values to a try

--- src/main/kotlin/tries/Try.kt
+++ src/main/kotlin/tries/Try.kt
@@ -2,4 +2,27 @@ package tries
 
 class Try<Key> {
     private val root = TryNode<Key>(key = null, parentNode = null)
+
+    /**
+     * @Insert
+     * Takes a lists and maps to a series of nodes
+     * Each item in the list represents a node in the try tree
+     *
+     * @since I don't I understand how this insertion process works. I assume it adds a complete letter,
+     * in which you have to add all the values iteratively
+    **/
+
+    fun insert(list: List<Key>){
+//        Keep track of the list
+        var current = root
+//        Store each element in a different node
+        list.forEach{element ->
+            if (current.children[element] == null){
+                current.children[element] = TryNode(element, current)
+            }
+            current = current.children[element]!!
+        }
+        //    The last node should be marked as the end of the node
+      current.isTerminating = true
+    }
 }
---
+++ src/main/kotlin/tries/TryMain.kt
@@ -0,0 +1,8 @@
+package tries
+
+fun main(){
+    val trie = Try<Char>()
+//    Insert cute to the try as list
+    trie.insert("cute".toList())
+
+}
