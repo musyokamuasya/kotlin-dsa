957133c9550a78611de02521ff6a2728b6332b82
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-04 18:54:18 +0300

    Create a basic foundation for the try node

---
+++ src/main/kotlin/tries/TryNode.kt
@@ -0,0 +1,11 @@
+package tries
+/**
+ * Tree that builds a prefix matching
+ * Used for searching words
+ * Form a word by going through a series of characters from root node to the final node
+ * A black dot is used to indicate the terminator
+ **/
+class TryNode <Key> (var key: Key?, var node: TryNode<Key>?){
+    var children : HashMap<Key, TryNode<Key>> = HashMap()
+    var isTerminating = false
+}
