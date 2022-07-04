9b1c476f64152b2351c238715c335e969a3ba4da
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-05 00:53:18 +0300

    Start of binary search algorithm

---
+++ src/main/kotlin/binary_search/BinarySearch.kt
@@ -0,0 +1,13 @@
+package binary_search
+
+/**
+ * @Definition
+ * Index manipulation at constant time
+ * Use array and arraylist to do the operation
+ * Collection must be sorted
+ **/
+// Create a function that is accessible globally
+fun <T: Comparable<T>> ArrayList<T>.binarySearch(value: T, range: IntRange = indices) : Int?{
+
+    return null
+}
