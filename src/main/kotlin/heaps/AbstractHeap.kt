839a2d342b9ca6e9a21d471a74037fc5603e492f
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-05 03:08:33 +0300

    Add methods for right and left children indicies, and for parent index

--- src/main/kotlin/heaps/AbstractHeap.kt
+++ src/main/kotlin/heaps/AbstractHeap.kt
@@ -1,5 +1,17 @@
 package heaps
 
 abstract class AbstractHeap <Element>(): Heap<Element> {
+    var elements: ArrayList<Element> = ArrayList<Element>()
+    override val count: Int
+        get() = elements.size
     abstract fun compare(a: Element, b: Element): Int
+
+    override fun peek(): Element? {
+        return elements.first()
+    }
+
+    private fun rightChildindex(index: Int) = (2 * index ) + 1
+    private fun leftChildIndex(index: Int) = (2 * index ) + 2
+    private fun parentIndex(index: Int) = (index - 1)/2
+
 }
