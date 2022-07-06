b6bb3012fcdf6f7262a3a9629e13771bb9bd162b
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-07 00:52:31 +0300

    Testing swapping. Working with a few error @sorting happening once

--- src/main/kotlin/sorting/bubble/SortingMain.kt
+++ src/main/kotlin/sorting/bubble/SortingMain.kt
@@ -1,2 +1,8 @@
 package sorting.bubble
 
+fun main(){
+    val list = arrayListOf<Int>(1, 2, 8, 3, 10, 6, 9,0, 4, 5, 7)
+    println("Original list $list")
+    list.bubbleSort(true)
+    println("Bubble sorted $list")
+}
