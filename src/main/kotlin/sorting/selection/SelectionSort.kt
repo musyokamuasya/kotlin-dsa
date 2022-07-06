d320b0e80bf143faec0f94206a4f34acd1bfc00e
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-07 01:21:01 +0300

    Selection sort algo

---
+++ src/main/kotlin/sorting/selection/SelectionSort.kt
@@ -0,0 +1,29 @@
+package sorting.selection
+
+import sorting.bubble.swapAt
+/**
+ * @SelectionSort
+ * Performs sorting in all elements
+ * Starts with the smallest element, then to the second smallest element until the second last element
+ *
+ * */
+fun <T: Comparable<T>> ArrayList<T>.selectionSort(
+    showPasses: Boolean = false
+){
+    if (this.size < 2) return
+
+    for(current in 0 until (this.size -1)){
+        var lowest = current
+
+        for (other in (current + 1) until this.size){
+            if (this[lowest] > this[other]){
+                lowest = other
+            }
+        }
+        if (lowest != current){
+            this.swapAt(lowest, current)
+        }
+        if (showPasses) println(this)
+    }
+
+}
