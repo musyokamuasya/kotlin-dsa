941c428f16c63999aca4d277ee791f0ddf3567ee
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-07 01:42:31 +0300

    Comments on insertion sort

--- src/main/kotlin/sorting/insertion/InsertionSort.kt
+++ src/main/kotlin/sorting/insertion/InsertionSort.kt
@@ -7,7 +7,9 @@ import sorting.bubble.swapAt
  * Shift the values to the left until they reach their optimal positions
  * */
 fun <T: Comparable<T>>ArrayList<T>.insertionSort(showPasses: Boolean = false){
+//    If the number is less than 2, no sorting
     if (this.size < 2) return
+//
     for (current in 1 until this.size){
         for (shifting in (1..current).reversed()){
             if (this[shifting] < this[shifting -1]){
