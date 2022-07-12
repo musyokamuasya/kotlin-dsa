c440155a7be348975437ae2b761f14f0a4ea3405
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-12 19:05:07 +0300

    Reversing list

--- src/main/kotlin/ds/List.kt
+++ src/main/kotlin/ds/List.kt
@@ -8,4 +8,9 @@ fun main(){
     val list: List<Int?> = listOf(1, 2,3, null, 4, 5, 6)
 
     println(list.subList(fromIndex = 1, toIndex = 4))
+    println(list.size)
+    println("First element is ${list[0]}")
+    println("Last element is ${list[list.size -1]}")
+    println("Reversed list ${list.reversed()}")
+
 }
