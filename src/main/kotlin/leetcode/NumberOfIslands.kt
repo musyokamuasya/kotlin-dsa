a3c9890af4223e813aadae927dc19768dcd2bb43
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-17 23:36:11 +0300

    Number of islands kotlin implementation

---
+++ src/main/kotlin/leetcode/NumberOfIslands.kt
@@ -0,0 +1,29 @@
+class NumberOfIslands {
+    fun numIslands(grid: Array<CharArray>?): Int {
+        if (grid == null || grid.size == 0) {
+            return 0
+        }
+        var numIslands = 0
+        for (i in 0..grid.size) {
+            for (j in 0..grid[i].size) {
+//                 Recursively call dfs
+                if (grid[i][j].equals('1')) {
+                    numIslands += dfs(grid, i, j)
+                }
+            }
+        }
+        return 0
+    }
+
+    fun dfs(grid: Array<CharArray>, i: Int, j: Int): Int {
+        if (i < 0 || j < 0 || i >= grid.size || j >= grid[i].size || grid[i][j].equals('0')) {
+            return 0
+        }
+        grid[i][j] = '0'
+        dfs(grid, i + 1, j)
+        dfs(grid, i - 1, j)
+        dfs(grid, i, j + 1)
+        dfs(grid, i, j - 1)
+        return 1
+    }
+}
