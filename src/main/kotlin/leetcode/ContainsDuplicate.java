fb192b042a1a11505ae8ea548208c267924d176c
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-17 23:39:21 +0300

    Java implementation of contains keyword

---
+++ src/main/kotlin/leetcode/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+/**
+ * Java implementation for checking duplicate number
+ * */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> uniques = new HashSet<>();
+        for (int num : nums) {
+            if (uniques.contains(num)) {
+                return true;
+            }
+            uniques.add(num);
+        }
+        return false;
+    }
+}
