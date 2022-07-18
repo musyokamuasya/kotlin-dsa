90d626f53bd28beec0b173c1dda8eb2e2b34e6a2
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-18 22:54:57 +0300

    Arrays - Initialization

---
+++ src/main/kotlin/arrays/BasicArray.kt
@@ -0,0 +1,33 @@
+package arrays
+
+import kotlin.Array
+
+/**
+ * Playing around with arrays
+ * 1. Operations
+ * 2. Dynamic Arrays
+ * 3. Immutable Arrays
+ * 4. Multidimensional Arrays and their Operations
+ * 5. Strings and related operations
+ * */
+
+class Array <T> private constructor(){
+//    val size: Int
+//    operator fun get(index : Int) : T
+//    operator fun set(index: Int, value: T): Unit
+//    operator fun iterator: Iterable<T>
+
+/**
+ * Different array initialization methods
+ * */
+
+//    Inferred string type
+    val friends = arrayOf("Ken", "Keith", "Ana", "Mary", "Cate")
+
+//    Inferred of type any
+    val person = arrayOf("Musyoka", 23, "josephmuasya8@gmail.com", 3.8)
+
+//    Creating a signed array
+    val person2: Array<String> = arrayOf("Musyoka","23", "josephmuasya8@gmail.com", "3.8")
+}
+
