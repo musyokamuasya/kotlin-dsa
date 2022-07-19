6bfc740f4a92177e383c2a27c73a336866957356
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-20 02:44:50 +0300

    Comments

--- src/main/kotlin/arrays/MultidimensionalArrays.kt
+++ src/main/kotlin/arrays/MultidimensionalArrays.kt
@@ -6,7 +6,12 @@ fun multidimensionalArrays() {
         arrayOf(4, 5, 6),
         arrayOf(7, 8, 9)
     )
-
+    /**
+     * How you call out multidimensional array.
+     * Start an outer loop
+     * Call the inner loop
+     * They have array O(n^2)
+     * */
     for (number in numbers){
         for(item in number){
             println(item)
