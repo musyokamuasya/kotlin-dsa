0a0e0b89602cbb0a2394118216cfde0131aaf390
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-20 04:42:33 +0300

    LinkedList instantiation

---
+++ src/main/kotlin/linkedlist/LinkyList.kt
@@ -0,0 +1,14 @@
+package linkedlist
+
+/**
+ * Another implementation of the LinkedList Data Structure.
+ * Each node has reference to the next node
+ * A node can hold multiple things, but at least the element and the next element
+ * */
+class LinkyList <E>{
+    private inner class Node<E> constructor(internal var element: E, internal var next: Node<E>?)
+
+    private var size = 0
+    private var head: Node<E>? = null
+    private var tail: Node<E>? = null
+}
