d63480664665286a9e81aa6d630c3f1c7d0117aa
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-21 03:06:48 +0300

    Insert element at particular location in linkedlist

--- src/main/kotlin/linkedlist/LinkyList.kt
+++ src/main/kotlin/linkedlist/LinkyList.kt
@@ -1,5 +1,8 @@
 package linkedlist
 
+
+
+
 /**
  * Another implementation of the LinkedList Data Structure.
  * Each node has reference to the next node
@@ -38,6 +41,56 @@ class LinkyList <E>{
         }
 
         t?.next = newNode
+
+        size ++
     }
 
+    /**
+     * Inserting node at a given index
+     * Get reference to the node at the given index
+     * The previous element would be the tail, and next node to previous will be the new next.
+     *
+     * */
+    fun insertAt(index: Int, element: E){
+        if (index <0 || index > size){
+            throw IndexOutOfBoundsException()
+        }
+//        If the index is at the top, just insert as inserting to tail
+        var x = head
+        val previousIndex = index -1
+        val nextIndex = index + 1
+        for (i in 0 until previousIndex){
+            x = x!!.next
+        }
+
+        val newNode = Node(element, null) // This is wrong
+    }
+
+//    Function to add all elements to the linked list
+        fun <T> addAll(index: Int, array: Array<T>): Boolean where T: E{
+    //    Check if the number is less than index, or whether it is larger than the size of the array
+        if (index <0 || index >= array.size){
+            throw IndexOutOfBoundsException()
+        }
+        if (array.isEmpty()) return false
+            val previous: Node<E>?
+            val nextNode: Node<E>?
+
+            when(index){
+                0 -> {
+                    previous = null
+                    nextNode = head
+                    }
+                size -> {
+                    previous = tail
+                    nextNode = null
+                }
+                else ->{
+//                    Configure the next and previous nodes
+//                    previous =
+//                    nextNode = previous.next
+                }
+            }
+            return true
+        }
 }
