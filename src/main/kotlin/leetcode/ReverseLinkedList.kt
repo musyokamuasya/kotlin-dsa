638d0ef650545c9181462c81c26828c08de3b8fe
Musyoka Muasya <josephmuasya8@gmail.com>
2022-07-25 04:36:04 +0300

    Make linkedlist function work

--- src/main/kotlin/leetcode/ReverseLinkedList.kt
+++ src/main/kotlin/leetcode/ReverseLinkedList.kt
@@ -7,13 +7,15 @@ import java.util.LinkedList
  * Reverse linked list using two common methods used to reverse a list
  * Using recursion
  * Two pointer method. The method below is for a two-pointer
+ *
+ * TODO Make this function work
  * */
 class ReverseLinkedList<T> {
     fun reverseList(head: LinkedList<T>?): LinkedList<T>? {
         return rev(head, null)
     }
 
-    fun rev(node: LinkedList<T>?, pre: LinkedList<T>?): LinkedList<T>? {
+    private fun rev(node: LinkedList<T>?, pre: LinkedList<T>?): LinkedList<T>? {
         val next: LinkedList<T>? = null
         if (node == null) return pre
         val temp: LinkedList<T> = node
